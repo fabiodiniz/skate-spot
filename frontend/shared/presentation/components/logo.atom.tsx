12cc186914241a5589765b0fe42b49074bc64d89
Fábio DIniz <contato@fabiodiniz.com.br>
2022-03-20 19:09:30 -0300

    ✨ Create basic components (button, input, spacer, logo)

---
+++ frontend/shared/presentation/components/logo.atom.tsx
@@ -0,0 +1,17 @@
+import { Image, ImageProps, ImageSourcePropType } from 'react-native'
+
+type Props = Omit<ImageProps, 'source'> & {
+  source?: ImageSourcePropType
+}
+
+const LogoAtom: React.FC<Props> = props => {
+  return (
+    <Image
+      testID="LogoAtom"
+      source={require('shared/presentation/statics/logo.png')}
+      {...props}
+    />
+  )
+}
+
+export default LogoAtom
