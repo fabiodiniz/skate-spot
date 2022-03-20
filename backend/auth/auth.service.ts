c579be38136932c004e76470f1169f7f836e6983
Fábio DIniz <contato@fabiodiniz.com.br>
2022-03-20 02:04:08 -0300

    🚚 Move some files around

--- backend/auth/auth.service.ts
+++ backend/auth/auth.service.ts
@@ -7,11 +7,13 @@ export class AuthService {
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(email)
+
     if (user && user.password === pass) {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user
       return result
     }
+
     return null
   }
 }
