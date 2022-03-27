22eac0d0a4be8a14f9226270e9c434944236708c
Fábio DIniz <contato@fabiodiniz.com.br>
2022-03-27 01:56:31 -0300

    🚧 Start login with Firebase

--- auth/application/login.factory.tsx
+++
@@ -1,9 +0,0 @@
-import LoginPage from 'auth/presentation/components/login.page'
-
-import React from 'react'
-
-export default class LoginFactory extends React.Component implements Factory {
-  render() {
-    return <LoginPage />
-  }
-}
