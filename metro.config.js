52fde00ea4465b848b3f26070d0d71b2a66c6031
Fábio Diniz <contato@fabiodiniz.com.br>
2022-10-11 20:40:33 -0400

    ♻️ Change little things about React components

---
+++ metro.config.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { getDefaultConfig } = require('expo/metro-config')
+const defaultConfig = getDefaultConfig(__dirname)
+defaultConfig.resolver.sourceExts.push('cjs')
+module.exports = defaultConfig
